a75ba3eb8e52214c616eea3073c226743185b9e8
aes011204 <aes011204@gmail.com>
2025-09-06 21:42:46 +0900

    feat : re :Line, LinMgr, PlayerJump, LineCol

--- Platformer_WinAPI/CStage04.cpp
+++ Platformer_WinAPI/CStage04.cpp
@@ -13,13 +13,13 @@ CStage04::~CStage04()
 
 void CStage04::Initialize()
 {
-//	LINEPOINT tPoint[2] =
-//	{
-//		{ 50.f, WINCY - 50.f },
-//		{ WINCX - 50.f, WINCY - 50.f }
-//	};
-//
-//	m_LineList.push_back(new CLine(tPoint[0], tPoint[1]));
+	LINEPOINT tPoint[2] =
+	{
+		{ 50.f, WINCY - 50.f },
+		{ WINCX - 50.f, WINCY - 50.f }
+	};
+
+	m_LineList.push_back(new CLine(tPoint[0], tPoint[1]));
 }
 
 void CStage04::Update()
@@ -43,31 +43,31 @@ void CStage04::Release()
 	m_LineList.clear();
 }
 
-//bool CStage04::Collision_Line(float& rX, float& rY)
-//{
-//	if (m_LineList.empty())
-//		return false;
-//
-//	CLine* pTargetLine = nullptr;
-//
-//	for (auto& pLine : m_LineList)
-//	{
-//		if (rX >= pLine->Get_Info().tLeft.fPosX
-//			&& rX < pLine->Get_Info().tRight.fPosX)
-//			pTargetLine = pLine;
-//	}
-//
-//	if (!pTargetLine)
-//		return false;
-//
-//	float x1 = pTargetLine->Get_Info().tLeft.fPosX;
-//	float y1 = pTargetLine->Get_Info().tLeft.fPosY;
-//
-//	float x2 = pTargetLine->Get_Info().tRight.fPosX;
-//	float y2 = pTargetLine->Get_Info().tRight.fPosY;
-//
-//	rY = ((y2 - y1) / (x2 - x1)) * (rX - x1) + y1;
-//	//! Y = (기울기) * (X - x1) + y1 ((x1, y1) = 직선 위의 한 점)
-//
-//	return true;
-//}
+bool CStage04::Collision_Line(float& rX, float& rY)
+{
+	if (m_LineList.empty())
+		return false;
+
+	CLine* pTargetLine = nullptr;
+	
+	for (auto& pLine : m_LineList)
+	{
+		if (rX >= pLine->Get_Info().tLeft.fPosX
+			&& rX < pLine->Get_Info().tRight.fPosX)
+			pTargetLine = pLine;
+	}
+
+	if (!pTargetLine)
+		return false;
+
+	float x1 = pTargetLine->Get_Info().tLeft.fPosX;
+	float y1 = pTargetLine->Get_Info().tLeft.fPosY;
+
+	float x2 = pTargetLine->Get_Info().tRight.fPosX;
+	float y2 = pTargetLine->Get_Info().tRight.fPosY;
+
+	rY = ((y2 - y1) / (x2 - x1)) * (rX - x1) + y1;
+	//! Y = (기울기) * (X - x1) + y1 ((x1, y1) = 직선 위의 한 점)
+
+	return true;
+}
